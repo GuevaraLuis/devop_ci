75cf3bf540c7862ae228cfa5f852d00397844b36
luis guevara <luis.guevara@gmail.com>
2021-11-29 22:40:38 -0300

    modificacion en test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	String meessage= "default message cambio test";
+	String meessage= "default message cambio test devops";
 	Assert.assertNotNull(message);
 	}
 
